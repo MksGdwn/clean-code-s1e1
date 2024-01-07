baf849698385d04af6cc7bf5351a836531df570f
mksgdwn <mksgdwn@gmail.com>
2024-01-07 15:30:35 +0500

    refactor(extended-1.2): add alternative for multimedia

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="btn delete";
   deleteButtonImg.src='./remove.svg';
   deleteButtonImg.className="btn-image";
+  deleteButtonImg.alt="delete";
   deleteButton.appendChild(deleteButtonImg);
 
 
